e92cb320961eda4c813be5e44b2778f5545c71a3
Lorenzo Gradaschi <lorenzogradaschi@gmail.com>
2024-03-14 00:20:03 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace PseudoRandomGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Izberite algoritem";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Izberite Seeding method";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Izberite ali vnesite ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 138);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Vnesite ime izhodne datoteke ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 168);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(228, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Vnesite začetno vrednost (med 0 in 1)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 243);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "GENERATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(197, 18);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(101, 20);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "osnovi časa";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(319, 19);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(123, 20);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "osnovi entropije";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(43, 8);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(453, 54);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(302, 21);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 20);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Saw funkcija";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(163, 21);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(110, 20);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Kotna funkcija";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Location = new System.Drawing.Point(43, 65);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(453, 52);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(221, 136);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(315, 166);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(79, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 201);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(173, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Vnesite nastavljeno velikost";
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(511, 18);
+            this.chart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(400, 256);
+            this.chart.TabIndex = 15;
+            this.chart.Text = "chart1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(221, 199);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(79, 22);
+            this.textBox3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 288);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace PseudoRandomGenerator
+{
+    public partial class Form1 : Form
+    {
+        private long seed;
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+            seed = DateTime.Now.Ticks;
+        }
+
+        static int TimeSeed()
+        {
+            return (int)(DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond);
+        }
+
+        
+
+        public int EntropySeed()
+        {
+
+            return (int)seed;
+
+        }
+
+        
+        static double AngularFunction(double x, int t)
+        {
+            double a = 1.61803398875;
+            double b = 1 - a;
+            double y = (x + t) % 1;
+            y = (a * y + b) % 1;
+            return y;
+        }
+       
+        static double SawFunction(double x, int t)
+        {
+            double y = (x + t) % 1;
+            if (y < 0.5)
+            {
+                y = 2 * y;
+            }
+            else
+            {
+                y = 2 * (1 - y);
+            }
+            return y;
+        }
+
+   
+        static List<int> GeneratePseudoRandomNumbers(Func<double, int, double> algorithm, Func<int> seedMethod, double initialValue, int count)
+        {
+            // Ustvarm seme
+            int t = seedMethod();
+            double x = initialValue;
+
+            List<int> numbers = new List<int>();
+            
+            for (int i = 0; i < count; i++)
+            {
+                
+                double y = algorithm(x, t);
+               
+                t = seedMethod();
+                x = y;
+                
+                numbers.Add((int)(y * 10) + 1); 
+            }
+            return numbers;
+        }
+
+        
+        static int FindRecurrencePeriod(List<int> numbers)
+        {
+            
+            Dictionary<int, int> firstOccurrence = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (!firstOccurrence.ContainsKey(numbers[i]))
+                {
+                    firstOccurrence[numbers[i]] = i;
+                }
+            }
+           
+            int maxPeriod = numbers.Count / 2;
+            for (int period = 1; period <= maxPeriod; period++)
+            {
+                bool isPeriodic = true;
+                for (int i = 0; i < numbers.Count - period; i++)
+                {
+                    if (numbers[i] != numbers[i + period])
+                    {
+                        isPeriodic = false;
+                        break;
+                    }
+                }
+                if (isPeriodic)
+                {
+                    return period;
+                }
+            }
+            return -1;
+        }
+
+        
+        static void WriteNumbersToFile(List<int> numbers, string filename)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                foreach (int number in numbers)
+                {
+                    writer.WriteLine(number);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Func<double, int, double> algorithm;
+
+            if (radioButton1.Checked)
+            {
+                algorithm = AngularFunction;
+            }
+            else if (radioButton2.Checked)
+            {
+                algorithm = SawFunction;
+            }
+            else
+            {
+                MessageBox.Show("Algorithm not selected");
+                return;
+            }
+            Func<int> seedMethod;
+
+            if (radioButton3.Checked)
+            {
+                seedMethod = TimeSeed;
+            }
+            else if (radioButton4.Checked)
+            {
+                seedMethod = EntropySeed;
+            }
+            else
+            {
+                MessageBox.Show("No method is selected");
+                return;
+            }
+            double initialValue;
+            if (Double.TryParse(textBox2.Text, out initialValue))
+            {
+                if (initialValue > 1 || initialValue < 0)
+                {
+                    MessageBox.Show("The initial value must be between 0 and 1");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("The initial value must be between 0 and 1");
+                return;
+            }
+            int count = int.Parse(textBox3.Text);
+            if (count < 0)
+            {
+                MessageBox.Show("Invalid set size");
+                return;
+            }
+
+            List<int> numbers = GeneratePseudoRandomNumbers(algorithm, seedMethod, initialValue, count);
+
+            
+            int period = FindRecurrencePeriod(numbers);
+            if (period == -1)
+            {
+                MessageBox.Show("The sequence is aperiodically");
+            }
+            else
+            {
+                MessageBox.Show("The recurrence period is " + period + ".");
+            }
+
+            
+            string filename = textBox1.Text;
+            if (!string.IsNullOrEmpty(filename))
+            {
+                WriteNumbersToFile(numbers, filename);
+            }
+
+            
+            Dictionary<int, int> frequencyDistribution = new Dictionary<int, int>();
+
+           
+            foreach (int number in numbers)
+            {
+                if (frequencyDistribution.ContainsKey(number))
+                {
+                    frequencyDistribution[number]++;
+                }
+                else
+                {
+                    frequencyDistribution.Add(number, 1);
+                }
+            }
+
+            
+            chart.Size = new Size(600, 400);
+
+           
+            ChartArea chartArea = new ChartArea();
+            chartArea.AxisX.Title = "Number";
+            chartArea.AxisY.Title = "Frequency";
+            chart.ChartAreas.Add(chartArea);
+
+            
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Column;
+            foreach (int number in frequencyDistribution.Keys)
+            {
+                series.Points.AddXY(number, frequencyDistribution[number]);
+            }
+            chart.Series.Add(series);
+
+           
+            chart.SaveImage("frequency_distribution.png", ChartImageFormat.Png);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
